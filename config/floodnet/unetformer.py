cfea20e6a2fecd122c5da7e439fe79e44afd433f
manhhv87 <manhhv87@gmail.com>
2023-10-31 23:43:07 +0700

    modify

--- config/floodnet/unetformer.py
+++ config/floodnet/unetformer.py
@@ -10,7 +10,6 @@ max_epoch = 105
 ignore_index = len(CLASSES)
 train_batch_size = 8
 val_batch_size = 8
-test_batch_size = 8
 lr = 6e-4
 weight_decay = 0.01
 backbone_lr = 6e-5
@@ -28,8 +27,7 @@ save_top_k = 1
 save_last = True
 check_val_every_n_epoch = 1
 pretrained_ckpt_path = None  # the path for the pretrained model weight
-# default or gpu ids:[0] or gpu nums: 2, more setting can refer to pytorch_lightning
-gpus = 'auto'
+gpus = 'auto'   # default or gpu ids:[0] or gpu nums: 2, more setting can refer to pytorch_lightning
 resume_ckpt_path = None  # whether continue training with the checkpoint, default None
 
 #  define the network
@@ -40,12 +38,11 @@ loss = UnetFormerLoss(ignore_index=ignore_index)
 use_aux_loss = True
 
 # define the dataloader
-train_dataset = FloodNetDataset(
-    data_root='data/floodnet/train', mode='train', mosaic_ratio=0.25, transform=train_aug)
-val_dataset = FloodNetDataset(
-    data_root='data/floodnet/val', mode='train', transform=val_aug)
-test_dataset = FloodNetDataset(
-    data_root='data/floodnet/test')
+train_dataset = FloodNetDataset(data_root='data/floodnet/train', mode='train', 
+                                mosaic_ratio=0.25, transform=train_aug)
+val_dataset = FloodNetDataset(data_root='data/floodnet/val', mode='val', 
+                              transform=val_aug)
+test_dataset = FloodNetDataset(data_root='data/floodnet/test')
 
 train_loader = DataLoader(dataset=train_dataset,
                           batch_size=train_batch_size,
@@ -61,16 +58,9 @@ val_loader = DataLoader(dataset=val_dataset,
                         pin_memory=True,
                         drop_last=False)
 
-test_loader = DataLoader(dataset=test_dataset,
-                         batch_size=test_batch_size,
-                         num_workers=2,
-                         shuffle=False,
-                         pin_memory=True,
-                         drop_last=False)
-
 # define the optimizer
 layerwise_params = {
-    "backbone.*": dict(lr=backbone_lr, weight_decay=backbone_weight_decay)}
+"backbone.*": dict(lr=backbone_lr, weight_decay=backbone_weight_decay)}
 net_params = utils.process_model_params(net, layerwise_params=layerwise_params)
 base_optimizer = torch.optim.AdamW(
     net_params, lr=lr, weight_decay=weight_decay)
